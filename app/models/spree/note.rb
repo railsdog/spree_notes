29f93489daa78be3f859fdf7a254e27b1917937b
mkcode <chrisewald@gmail.com>
2014-07-09 19:19:27 -0400

    Use new block syntax for default_scope

--- app/models/spree/note.rb
+++ app/models/spree/note.rb
@@ -1,7 +1,7 @@
 class Spree::Note < ActiveRecord::Base
   belongs_to :noteable, polymorphic: true
   validates_presence_of :author
-  default_scope order(important: :desc, created_at: :desc)
+  default_scope { order(important: :desc, created_at: :desc) }
 
   def self.important
     where(important: true)
